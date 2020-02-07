8f4bb7c4e1d9f0f9ea0037b71e9be1a1b85dd30c
Joey <joeyskuo@gmail.com>
2020-02-06 21:50:26 -0800

    added bodyParser middleware

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded());
 
 app.use((req, res, next) => {
     res.send('Express Server Running');
