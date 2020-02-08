e20cafec0aac9b59d74075831e3b9cf555d5a59d
Joey <joeyskuo@gmail.com>
2020-02-07 18:54:50 -0800

    comments added to label sections

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Express Server
 const app = express();
 
+// Routes
 const mainRoutes = require('./routes/admin');
+
+// Middleware
 app.use(bodyParser.urlencoded());
 
 // Routes
 app.use(mainRoutes);
 
+// Server Start
 app.listen(3000);
