5febb2822c247d87dbdb24d56b2bc572dc6392b3
Joey <joeyskuo@gmail.com>
2020-02-14 15:49:33 -0800

    Product creation method added to product controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,3 +1,11 @@
+const Product = require('../models/Product');
+
 exports.getAddProduct = (req, res, next) => {
     res.status(200).render('add-product', {pageTitle: 'Admin'});
 };
+
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect('/');
+}
