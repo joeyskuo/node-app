d8fb6a9d7af13b6b9e8078518faf737c706fbd5d
Joey <joeyskuo@gmail.com>
2020-02-14 15:50:52 -0800

    Product creation POST route added to routes

--- routes/admin.js
+++ routes/admin.js
@@ -5,5 +5,6 @@ const productsController = require('../controllers/products');
 const router = express.Router();
 
 router.get('/', productsController.getAddProduct);
+router.post('/add-product', productsController.postAddProduct);
 
 module.exports = router;
