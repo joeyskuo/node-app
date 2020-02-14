0ff185d62be0aba726aa2bbe163d868428a17317
Joey <joeyskuo@gmail.com>
2020-02-14 15:37:12 -0800

    Product persistence and fetch methods added

--- models/Product.js
+++ models/Product.js
@@ -1,8 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
 class Product {
     constructor(title) {
         this.title = title;
-
     }
+
+    save() {
+        const productsFile = path.join(path.dirname(process.mainModule.filename),
+        'data',
+        'products.json');
+
+        fs.readFile(productsFile, (err, data) => {
+            let products = [];
+            if(!err) {
+                products = JSON.parse(data);
+            }
+            products.push(this);
+
+            fs.writeFile(productsFile, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        })
+    }
+
+    static fetchAll() {
+        
+    }
+
 }
 
 module.exports = Product;
